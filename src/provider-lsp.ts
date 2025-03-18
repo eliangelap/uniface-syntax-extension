8b7d7ec31cb48360b8a5ef7c1eeb2c3b113c44ca
Eliangela M Palharini Paes <emenezes@coamo.com.br>
2025-03-18 20:41:50 -0300

    fix: 🐛 Fix variables search
    Fix variables search when single line declaration

--- src/provider-lsp.ts
+++ src/provider-lsp.ts
@@ -1,127 +1,134 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 function getBlockAroundPosition(
-  document: vscode.TextDocument,
-  position: vscode.Position
+    document: vscode.TextDocument,
+    position: vscode.Position
 ) {
-  const text = document.getText();
-  const cursorOffset = document.offsetAt(position);
+    const text = document.getText();
+    const cursorOffset = document.offsetAt(position);
 
-  // Localiza o início do bloco (entry|trigger) antes do cursor
-  let blockStart = text.lastIndexOf("entry ", cursorOffset);
-  const triggerStart = text.lastIndexOf("trigger ", cursorOffset);
-  const operationStart = text.lastIndexOf("operation ", cursorOffset);
-  const functionStart = text.lastIndexOf("function ", cursorOffset);
+    // Localiza o início do bloco (entry|trigger) antes do cursor
+    let blockStart = text.lastIndexOf('entry ', cursorOffset);
+    const triggerStart = text.lastIndexOf('trigger ', cursorOffset);
+    const operationStart = text.lastIndexOf('operation ', cursorOffset);
+    const functionStart = text.lastIndexOf('function ', cursorOffset);
 
-  blockStart = Math.max(
-    blockStart,
-    triggerStart,
-    operationStart,
-    functionStart
-  );
+    blockStart = Math.max(
+        blockStart,
+        triggerStart,
+        operationStart,
+        functionStart
+    );
 
-  if (blockStart < 0) {
-    return "";
-  }
+    if (blockStart < 0) {
+        return '';
+    }
 
-  // Localiza o fim do bloco (end) depois do cursor
-  let blockEnd = text.indexOf("end", cursorOffset);
+    // Localiza o fim do bloco (end) depois do cursor
+    let blockEnd = text.indexOf('end', cursorOffset);
 
-  if (blockEnd < 0) {
-    blockEnd = text.length;
-  }
+    if (blockEnd < 0) {
+        blockEnd = text.length;
+    }
 
-  const subString = text.substring(blockStart, blockEnd);
+    const subString = text.substring(blockStart, blockEnd);
 
-  if (
-    !subString.includes("variables") &&
-    !subString.includes("params") &&
-    !subString.includes("endvariables") &&
-    !subString.includes("endparams")
-  ) {
-    return "";
-  }
+    if (
+        !subString.includes('variables') &&
+        !subString.includes('params') &&
+        !subString.includes('endvariables') &&
+        !subString.includes('endparams')
+    ) {
+        return '';
+    }
 
-  const endRegex = /(^|\s)end(\s|;|$)/;
-  if (endRegex.test(subString)) {
-    return "";
-  }
+    const endRegex = /(^|\s)end(\s|;|$)/;
+    if (endRegex.test(subString)) {
+        return '';
+    }
 
-  return text.substring(blockStart, blockEnd);
+    return text.substring(blockStart, blockEnd);
 }
 
 export const completionItemProvider = (): vscode.CompletionItemProvider => {
-  return {
-    provideCompletionItems(document, position) {
-      return provideCompletionItems(document, position);
-    },
-  };
+    return {
+        provideCompletionItems(document, position) {
+            return provideCompletionItems(document, position);
+        },
+    };
 };
 
 export const provideCompletionItems = (
-  document: vscode.TextDocument,
-  position: vscode.Position
+    document: vscode.TextDocument,
+    position: vscode.Position
 ) => {
-  const completions = [];
+    const completions = [];
 
-  const entries: string[] = document.getText().match(/entry\s+(\w+)/gi) || [];
+    const entries: string[] = document.getText().match(/entry\s+(\w+)/gi) || [];
 
-  const lineText = document.lineAt(position).text;
+    const lineText = document.lineAt(position).text;
 
-  if (lineText.includes("call")) {
-    const entryItems = entries.map((entry) => {
-      const entryName = entry.replace("entry", "").trim();
-      return new vscode.CompletionItem(
-        entryName,
-        vscode.CompletionItemKind.Method
-      );
-    });
-    completions.push(...entryItems);
+    if (lineText.includes('call')) {
+        const entryItems = entries.map((entry) => {
+            const entryName = entry.replace('entry', '').trim();
+            return new vscode.CompletionItem(
+                entryName,
+                vscode.CompletionItemKind.Method
+            );
+        });
+        completions.push(...entryItems);
+
+        return completions;
+    }
+
+    const blockText = getBlockAroundPosition(document, position);
+
+    if (!blockText) {
+        return [];
+    }
+
+    const variableRegex = /variables([\s\S]*?)endvariables/gi;
+    const matchVariablesBlock = variableRegex.exec(blockText);
+
+    if (matchVariablesBlock) {
+        const varLines = matchVariablesBlock[1].split('\n');
+        for (const line of varLines) {
+            const varMatch = line
+                .trim()
+                .match(/(STRING|NUMERIC|HANDLE|STRUCT|BOOLEAN|IMAGE|ENTITY|RAW|DATE|DATETIME)\s+(\w+)/i);
+            if (varMatch) {
+                const variableNames = varMatch?.input?.replace(`${varMatch[1]}`, '')?.split(',');
+                if (variableNames) {
+                    for (const variableName of variableNames) {
+                        const item = new vscode.CompletionItem(
+                            variableName.trim(),
+                            vscode.CompletionItemKind.Variable
+                        );
+                        completions.push(item);
+                    }
+                }
+            }
+        }
+    }
+
+    const paramRegex = /params([\s\S]*?)endparams/gi;
+    const matchParamsBlock = paramRegex.exec(blockText);
+    if (matchParamsBlock) {
+        const paramLines = matchParamsBlock[1].split('\n');
+        for (const line of paramLines) {
+            const paramMatch = line
+                .trim()
+                .match(/(\$?\w+\$?)\s*:\s*(in|out|inout)/i);
+
+            if (paramMatch) {
+                const item = new vscode.CompletionItem(
+                    paramMatch[1],
+                    vscode.CompletionItemKind.Variable
+                );
+                completions.push(item);
+            }
+        }
+    }
 
     return completions;
-  }
-
-  const blockText = getBlockAroundPosition(document, position);
-
-  if (!blockText) {
-    return [];
-  }
-
-  const variableRegex = /variables([\s\S]*?)endvariables/gi;
-  const matchVariablesBlock = variableRegex.exec(blockText);
-
-  if (matchVariablesBlock) {
-    const varLines = matchVariablesBlock[1].split("\n");
-    for (const line of varLines) {
-      const varMatch = line
-        .trim()
-        .match(/(STRING|NUMERIC|STRUCT|BOOLEAN)\s+(\w+)/i);
-      if (varMatch) {
-        const item = new vscode.CompletionItem(
-          varMatch[2],
-          vscode.CompletionItemKind.Variable
-        );
-        completions.push(item);
-      }
-    }
-  }
-
-  const paramRegex = /params([\s\S]*?)endparams/gi;
-  const matchParamsBlock = paramRegex.exec(blockText);
-  if (matchParamsBlock) {
-    const paramLines = matchParamsBlock[1].split("\n");
-    for (const line of paramLines) {
-      const paramMatch = line.trim().match(/(\$?\w+\$?)\s*:\s*(in|out|inout)/i);
-
-      if (paramMatch) {
-        const item = new vscode.CompletionItem(
-          paramMatch[1],
-          vscode.CompletionItemKind.Variable
-        );
-        completions.push(item);
-      }
-    }
-  }
-
-  return completions;
 };
