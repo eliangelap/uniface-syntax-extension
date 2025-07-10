e7ac41202a9ac45f94f731b43e77b77462e183b5
Eliangela M Palharini Paes <emenezes@coamo.com.br>
2025-07-09 23:19:55 -0300

    fix: 🐛 fixed entries list
    fixed entries list in autocompletion
    
    ✅ Closes: #46

--- src/code/getEntriesCompletionList.use.case.ts
+++ src/code/getEntriesCompletionList.use.case.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { GetEntriesList } from "./getEntriesList.use.case";
-import { DeclaredModule } from './getVariablesFromBlock.use.case';
+import { DeclaredModule } from "./getVariablesFromBlock.use.case";
 
 export class GetEntriesCompletionList {
     public execute(
@@ -8,17 +8,15 @@ export class GetEntriesCompletionList {
         position: vscode.Position
     ): vscode.CompletionItem[] {
         const completions = [];
-        const declaredModules: DeclaredModule[] = new GetEntriesList().execute(document);
+        const declaredModules: DeclaredModule[] = new GetEntriesList().execute(
+            document
+        );
 
         const lineText = document.lineAt(position).text.trim();
 
         if (lineText.startsWith("call")) {
             const entryItems = declaredModules.map((entry) => {
-                const entryName = entry.name
-                    .trim()
-                    .split(" ")[1]
-                    .trim()
-                    .split(";")[0];
+                const entryName = entry.name.trim();
                 return new vscode.CompletionItem(
                     entryName,
                     vscode.CompletionItemKind.Method
